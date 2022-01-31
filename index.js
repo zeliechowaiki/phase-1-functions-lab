bfe575390b768a1320382a02d8fa26b0bad00902
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-01-31 17:07:19 -0500

    commit tree e867adb29e58ccefe0a1c29345fac3752a793127

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup-42);
+}
+function distanceFromHqInFeet(pickup) {
+    return (distanceFromHqInBlocks(pickup)*264);
+}
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(start-destination)*264);
+}
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance<=400) {
+        return 0;
+    }
+    else if (distance<=2000) {
+        return ((distance-400)*.02);
+    }
+    else if (distance <=2500) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }
+}
